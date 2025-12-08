f879f20d1adf446f91878d40161b451197282fe4
Sohail <sohailrasoolabbasi21@gmail.com>
2025-12-09 00:32:49 +0500

    UP

--- db/seed.ts
+++ db/seed.ts
@@ -14,10 +14,14 @@ const prisma = new PrismaClient({
 
 async function main() {
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log('Database seeded successfully');
 }
-
 main();
